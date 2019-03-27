9bc9f54d5c358b7d579a6c50c8ccbd23e3c098fe
Josh_Date <jjdate@live.com>
2019-03-27 17:16:26 -0400

    done

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
+var buttonOff = document.querySelector('.switch.on')
+var textOff = document.querySelector('.status')
+
+buttonOff.addEventListener('click', function(){
+    var color = document.body.style.backgroundColor 
+    //textOff.innerHTML = "Hey Who Turned Off The Lights!!!" 
+        if(color == 'black'){
+            buttonOff.addEventListener('click', function(){
+            document.body.style.backgroundColor = 'white'
+            textOff.innerHTML = "It's so bright in here!!"
+            })
+        }else {
+                buttonOff.addEventListener('click',function(){
+                document.body.style.backgroundColor = 'black'
+                textOff.innerHTML = "Hey Who Turned Off The Lights!!!" 
+            })
+
+}
+    
+})
+
 
 
